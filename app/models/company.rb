13bfdf38fbce4dcc732b21cf09bf354c0950c014
jensenmiers <jensenmiers@gmail.com>
2023-02-08 12:25:52 -0800

    90 minute commit

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,14 @@
 class Company < ActiveRecord::Base
+    
+    # def self.oldest_company
+    #     Company.minimum(:founding_year)
+    # end
+    
     has_many :freebies
     has_many :devs, through: :freebies
+    
+    # def give_freebie(dev, item_name, value)
+    #     self.freebies.create(item_name: item_name, value: value)
+    # end
+
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,4 +1,8 @@
 class Freebie < ActiveRecord::Base
     belongs_to :dev
     belongs_to :company
+
+    def print_details
+        "#{self.dev} owns a #{self.item_name} from #{self.company}"
+    end
 end
